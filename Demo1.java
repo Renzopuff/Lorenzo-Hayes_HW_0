60bc5c3071e79b7c88d5946ae861be5784e67f3f
Renzopuff <lorenzo.hayes@maine.edu>
2024-01-19 11:32:49 -0500

    Create Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,8 @@
+// Lorenzo Hayes Demo 1 for Java (FIRST LINES OF JAVA!)
+
+public class Test {
+    public static void main(string[] args){
+        Test2 x = new Test2();
+        System.out.println("hello world!");
+    }
+}
